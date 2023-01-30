48408ff0820fb3d90c8505d7405578819de9733d
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-30 14:55:30 +0400

    14 - Waiting For Many Task To Complete

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
     public static void main(String[] args) {
         var task1 = CompletableFuture.supplyAsync(() -> 20);
         var task2 = CompletableFuture.supplyAsync(() -> 0.9);
+        var task3 = CompletableFuture.supplyAsync(() -> 0.3);
 
-        task1.thenCombine(task2, (price, exchangeRate) -> price * exchangeRate)
-                .thenAccept(System.out::println);
+        var all = CompletableFuture.allOf(task1,task2,task3);
+        all.thenRun(() -> {
+            try {
+                System.out.println(task1.get());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("All Done!");
+        });
+
+
     }
 }
