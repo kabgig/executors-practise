060beb9527d0694198c3fe1f369d88a410924bba
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-30 15:02:57 +0400

    15 - Waiting For the first fastest task result

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,19 @@
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 public class Main {
     public static void main(String[] args) {
-        var task1 = CompletableFuture.supplyAsync(() -> 20);
-        var task2 = CompletableFuture.supplyAsync(() -> 0.9);
-        var task3 = CompletableFuture.supplyAsync(() -> 0.3);
-
-        var all = CompletableFuture.allOf(task1,task2,task3);
-        all.thenRun(() -> {
-            try {
-                System.out.println(task1.get());
-            } catch (InterruptedException | ExecutionException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("All Done!");
+        var task1 = CompletableFuture.supplyAsync(() -> {
+            LongTask.Simulate();
+            return 20;
         });
+        var task2 = CompletableFuture.supplyAsync(() -> {
+            LongTask.Simulate();
+            return 0.9;
+        });
+        var fastest = CompletableFuture.supplyAsync(() -> 0.3);
 
+        CompletableFuture.anyOf(task1, task2, fastest)
+                .thenAccept(System.out::println);
 
     }
 }
